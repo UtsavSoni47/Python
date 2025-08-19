a368b6d70646e8f220a3cec44dbf83616070fbe2
UtsavSoni47 <utsavsoni0407@gmail.com>
2025-08-19 11:46:18 +0530

    Add files via upload

---
+++ scatter.py
@@ -0,0 +1,9 @@
+import pylab as us
+# Make an array of x values
+x = [1, 2, 3, 4, 5]
+# Make an array of y values for each x value
+y = [1, 4, 9, 16, 25]
+# use pylab to plot x and y
+us.plot(x, y,'cp')
+# show the plot on the screen
+us.show()
